4275fb72fc81059367cc1bc1a66e6b1d95598c28
Michael Skalak <skalakm@dickinson.edu>
2019-10-17 08:56:50 -0400

    added exam practice

--- code/src/code/ExamPractice.java
+++ code/src/code/ExamPractice.java
@@ -2,4 +2,15 @@ package code;
 
 public class ExamPractice {
 
+	
+	public int find(int[][] array, int target) { // assuming a square array
+		for(int i = 0; i < array.length; ++i) {
+			for(int j = 0; j < array.length; ++j) {
+				if( (array[i]) [j] == target) {
+					return i +j ;
+				}
+			}
+		}
+		return -1;
+	}
 }
