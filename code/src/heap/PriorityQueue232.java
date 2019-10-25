9c07a99f89b55aa21be3fafd6b3aace23b5bcbaa
Michael Skalak <skalakm@dickinson.edu>
2019-10-25 11:20:56 -0400

    added remove

--- code/src/heap/PriorityQueue232.java
+++ code/src/heap/PriorityQueue232.java
@@ -16,6 +16,92 @@ public class PriorityQueue232< Key extends Comparable<Key>, Value> {
 		}
 	}
 	
+	public Value remove() {
+		
+		
+		// size 1 tree
+		if( tree.size() == 1) {
+			Value v = tree.get(0).value;
+			tree.remove(0);
+			return v;
+		}
+		
+		// get node to remove
+		HeapNode< Key, Value > node = tree.get(0);
+		
+		
+		// swap it to last leaf leaf
+		swap(0, tree.size()-1);
+		
+		
+		// remove it from tree
+		tree.remove(tree.size()-1);
+		
+		
+		//fix heap property 
+		
+			// trickle down from the root
+		trickleDown(0);
+		return node.value;
+		
+		
+	}
+	
+	public void swap(int index0, int index2) {
+		HeapNode<Key, Value> temp = tree.get(index0);
+		
+		tree.set(index0, tree.get(index2));
+		tree.set( index2, temp);
+		
+	}
+	
+	public void trickleDown(int index) {
+		
+		if(!isLeaf(index)) {
+			// find smaller child
+			int indexOfSmallerChild = getIndexOfSmallerChild(index);
+			
+			HeapNode<Key, Value> smallerChild = tree.get(indexOfSmallerChild);
+			
+			// if smaller child is less than current
+			if( smallerChild.key.compareTo( tree.get(index).key) < 0) {
+				//swap
+				swap(index, indexOfSmallerChild);
+				
+				// call trickle down recursively
+				trickleDown(indexOfSmallerChild);
+			}
+				
+			
+			
+		}
+	}
+	
+	private int getIndexOfSmallerChild(int index) {
+		int leftChildIndex = getLeftChild(index);
+		
+		//default is that left child is smaller
+		int smallerIndex = leftChildIndex;
+		
+		// if there is a right child
+		if(haveRightChild(index)) {
+			//check if right child is smaller
+			HeapNode<Key, Value> rightChild = tree.get(getRightChild(index));
+			HeapNode<Key, Value> leftChild = tree.get(leftChildIndex);
+			if( rightChild.key.compareTo(leftChild.key) < 0) {
+				smallerIndex = getRightChild(index);
+			}
+			
+		}
+		return smallerIndex;
+		
+	}
+	
+	public boolean isLeaf(int index) {
+		return haveLeftChild(index) || haveRightChild(index);
+	}
+	
+	
 	private boolean checkHeapProperty() {
 		return checkHeapProperty(0);
 	}
@@ -47,6 +133,8 @@ public class PriorityQueue232< Key extends Comparable<Key>, Value> {
 		}
 		
 	}
+	
+	
 	
 	
 	private int getLeftChild(int cur) {
